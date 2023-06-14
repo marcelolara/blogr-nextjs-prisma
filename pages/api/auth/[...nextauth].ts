b0ddf1526f7ab49f050f250ddcb3377909a7eebd
Marcelo Lara <chelop@gmail.com>
2023-06-14 14:53:42 -0400

    Adding nextauth secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,7 +11,7 @@ export const authOptions = {
     }),
   ],
   adapter: PrismaAdapter(prisma),
-  secret: process.env.SECRET
+  secret: process.env.NEXTAUTH_SECRET
 }
 
 export default NextAuth(authOptions)
